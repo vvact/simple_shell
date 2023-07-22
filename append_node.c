61c604399f9fea3030a17552e84f175573df12e2
Victor Midega <vbm7752@gmail.com>
2023-07-22 08:50:38 -0700

    updates

--- append_node.c
+++ append_node.c
@@ -1,67 +1,47 @@
 #include "sshell.h"
 /**
- * append_directories - Append path directories from a path variable
- * @path_variable: Pointer to the path variable
- * @path_list: Pointer to the path_t list
- *
- * Return: Pointer to the new list
+ * appendnode - append path var to an empty path_t
+ * @s: pointer to path var
+ * @lists: pointer to path_t list
+ * Return: pointer to the new list
  */
-path_t *append_directories(char *path_variable, path_t *path_list)
+path_t *appendnode(char *s, path_t *lists)
 {
-if (path_variable == NULL || *path_variable == '\0')
-return (path_list);
-path_t *head = path_list;
-path_t *tail = path_list;
-int start = 0, end = 0;
-while (path_variable[end] != '\0')
-{
-if (path_variable[end] == ':')
-{
-int dir_len = end - start;
-path_t *new_node = malloc(sizeof(path_t));
-if (new_node == NULL)
-{
-free_directory_list(head);
-return (NULL);
-}
-new_node->directory = malloc(dir_len + 2);
-if (new_node->directory == NULL)
-{
-free(new_node);
-free_directory_list(head);
-return (NULL);
-}
-strncpy(new_node->directory, path_variable + start, dir_len);
-new_node->directory[dir_len] = '/';
-new_node->directory[dir_len + 1] = '\0';
-new_node->next = NULL;
-if (tail == NULL)
-head = new_node;
-else
-tail->next = new_node;
-tail = new_node;
-start = ++end;
-}
-else
-{
-end++;
-}
-}
-return (head);
-}
+	char *directory;
+	path_t *pointer, *h;
+	int k = 0, n = 0, sup = 0, dlen = 0;
 
-/**
- * free_directory_list - Frees the linked list of directories
- * @path_list: Pointer to the path_t list
- */
-void free_directory_list(path_t *path_list)
-{
-while (path_list != NULL)
-{
-path_t *temp = path_list;
-path_list = path_list->next;
-free(temp->directory);
-free(temp);
+	if (s == NULL || lists == NULL)
+		return (NULL);
+	h = lists;
+	pointer = h;
+	while (pointer != NULL)
+	{
+		if (s[k] == ':' || s[k] == '\0')
+		{
+			if (s[k] != '\0')
+				k++;
+			directory = malloc(sizeof(char) * dlen + 2);
+			if (directory == NULL)
+				return (NULL);
+			while (s[sup] != ':' && s[sup] != '\0')
+			{
+				directory[n] = s[sup];
+				sup++;
+				n++;
+			}
+			directory[n++] = '/';
+			directory[n] = '\0';
+			sup = k;
+			n = 0;
+			pointer->directory = directory;
+			pointer = pointer->next;
+		}
+		else
+		{
+			dlen++;
+			k++;
+		}
+	}
+	return (h);
 }
-}
-
