07bf2cf7cebece113968f45b319b5e6caaf924ae
Victor Midega <vbm7752@gmail.com>
2023-07-23 15:46:26 -0700

    converts string to int

--- atois.c
+++ atois.c
@@ -1,22 +1,46 @@
 #include "sshell.h"
 
 /**
- * _env - deals with environmental variables
- * @env: double pointer to environmental variables
- * Return: 0 (success)
+ * atois - converts ASCII to integer
+ * @s: pointer to the string to be converted
+ * Return: integer
  */
-
-int _env(char **env)
+int atois(char *s)
 {
-	int k = 0;
-	int len = 0;
+	int index, length = 0;
+	int tenth = 1;
+	unsigned int integer_value = 0;
+	int is_negative = 0;
+	int negative_sign_count = 0;
+	int positive_sign_count = 0;
 
-	while (env[k] != NULL)
+	while (s[length] != '\0')
 	{
-		len = str_len(env[k]);
-		write(STDOUT_FILENO, env[k], len);
-		write(STDOUT_FILENO, "\n", 1);
-		k++;
+		if (s[length] > '9' || s[length] < '0')
+			return (-1);
+		length++;
 	}
-	return (0);
+	while ((s[index] > '9' || s[index] < '0') && s[index] != '\0')
+	{
+		if (s[index] == '-')
+			negative_sign_count++;
+		if (s[index] == '+')
+			positive_sign_count++;
+		index++;
+	}
+	if (s[index] == '\0')
+		return (0);
+	if ((negative_sign_count % 2) != 0)
+		is_negative = 1;
+	tenth = tens_point(index, s);
+	while (s[index] >= '0' && s[index] <= '9')
+	{
+		integer_value += ((s[index] - '0') * tenth);
+		tenth /= 10;
+		index++;
+	}
+	if (is_negative == 1)
+		integer_value *= -1;
+
+	return ((int)integer_value);
 }
