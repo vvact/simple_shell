95546c18c6388ed46c1cb6fda4af0ff8f049c015
Victor Midega <vbm7752@gmail.com>
2023-07-26 14:39:54 -0700

    appending nodes

--- append_node.c
+++ append_node.c
@@ -1,47 +1,41 @@
 #include "simple_shell.h"
 /**
- * appendnode - append path var to an empty path_t
- * @s: pointer to path var
- * @lists: pointer to path_t list
- * Return: pointer to the new list
+ * append_directories_to_list - Append path directories to.
+ * @path_var: Pointer to the path variable string.
+ * @list_head: Pointer to the head of the path_t list.
+ * Return: Pointer to the updated head of the linked list.
  */
-path_t *appendnode(char *s, path_t *lists)
+path_t *append_directories_to_list(char *path_var, path_t *list_head)
 {
-	char *directory;
-	path_t *pointer, *h;
-	int k = 0, n = 0, sup = 0, dlen = 0;
-
-	if (s == NULL || lists == NULL)
-		return (NULL);
-	h = lists;
-	pointer = h;
-	while (pointer != NULL)
-	{
-		if (s[k] == ':' || s[k] == '\0')
-		{
-			if (s[k] != '\0')
-				k++;
-			directory = malloc(sizeof(char) * dlen + 2);
-			if (directory == NULL)
-				return (NULL);
-			while (s[sup] != ':' && s[sup] != '\0')
-			{
-				directory[n] = s[sup];
-				sup++;
-				n++;
-			}
-			directory[n++] = '/';
-			directory[n] = '\0';
-			sup = k;
-			n = 0;
-			pointer->dir = directory;
-			pointer = pointer->next;
-		}
-		else
-		{
-			dlen++;
-			k++;
-		}
-	}
-	return (h);
+char *directory;
+path_t *current_node, *head;
+int i = 0, dir_len = 0, start = 0, end = 0;
+if (path_var == NULL || list_head == NULL)
+return (NULL);
+head = list_head;
+current_node = head;
+while (current_node != NULL)
+{
+if (path_var[end] == ':' || path_var[end] == '\0')
+{
+if (path_var[end] != '\0')
+end++;
+dir_len = end - start;
+directory = malloc(sizeof(char) * (dir_len + 2));
+if (directory == NULL)
+return (NULL);
+for (i = 0; i < dir_len; i++)
+directory[i] = path_var[start + i];
+directory[i++] = '/';
+directory[i] = '\0';
+start = end;
+current_node->dir = directory;
+current_node = current_node->next;
+}
+else
+{
+end++;
+}
+}
+return (head);
 }
