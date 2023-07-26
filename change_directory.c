33580f86142b2584eb2282bee16f8eac978fbec0
Victor Midega <vbm7752@gmail.com>
2023-07-26 14:56:16 -0700

    changing directory

--- change_directory.c
+++ change_directory.c
@@ -1,41 +1,48 @@
 #include "simple_shell.h"
-
 /**
  * change_dir - changes directory
  * @arr: double pointer to array of cmd inputs
- * @env: double pointer to the environment var
- * Return: 0 for success
+ * @env: double pointer to the environment variables
+ * Return: 0 for success, -1 for failure
  */
 int change_dir(char **arr, char **env)
 {
-	int k = 0;
-	char cw_dir[1024];
-	char *dir_new;
-
-	if (arr[1] == NULL)
-	{
-		if (chdir(get_env("ROOT", env)) == -1)
-		{
-			perror(arr[0]);
-			write(STDERR_FILENO, "cd: cannot cd to root\n", 22);
-		}
-	}
-	else
-	{
-		getcwd(cw_dir, 1024);
-		while (cw_dir[k] != '\0')
-			k++;
-		cw_dir[k++] = '/';
-		cw_dir[k] = '\0';
-		dir_new = str_concat(cw_dir, arr[1]);
-		if (dir_new == NULL)
-			return (0);
-		if (chdir(dir_new) == -1)
-		{
-			perror(arr[0]);
-			write(STDERR_FILENO, "DIR cant be found\n", 18);
-		}
-		free(dir_new);
-	}
-	return (0);
+char cw_dir[1024];
+char *dir_new;
+if (arr[1] == NULL)
+{
+char *home_dir = get_env("HOME", env);
+if (home_dir == NULL)
+{
+write(STDERR_FILENO, "cd: No HOME environment variable.\n", 33);
+return (-1);
+}
+if (chdir(home_dir) == -1)
+{
+perror(arr[0]);
+free(home_dir);
+return (-1);
+}
+free(home_dir);
+}
+else
+{
+getcwd(cw_dir, sizeof(cw_dir));
+dir_new = str_concat(cw_dir, "/");
+if (dir_new == NULL)
+return (-1);
+char *new_path = str_concat(dir_new, arr[1]);
+free(dir_new);
+if (new_path == NULL)
+return (-1);
+if (chdir(new_path) == -1)
+{
+perror(arr[0]);
+write(STDERR_FILENO, "cd: Cannot change directory.\n", 29);
+free(new_path);
+return (-1);
+}
+free(new_path);
+}
+return (0);
 }
