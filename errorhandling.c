856c6c60a84b9a9b795ef815d6e4de3a2a792912
Victor Midega <vbm7752@gmail.com>
2023-07-26 15:56:51 -0700

    error handling

--- errorhandling.c
+++ errorhandling.c
@@ -1,26 +1,26 @@
 #include "simple_shell.h"
 
 /**
- * error_hand - Dealing with errors
- * @argv: argument vector
- * @arr: pointer to token array from user
+ * error_hand - handles errors
+ * @argv: argument vectors
+ * @array: pointer to token array from user input
  * @cmdnum: number of cmd inputs
  * @l: user line input
  * @new_l: user input lacking '\n' character
  */
-void error_hand(char **argv, char **arr, int cmdnum, char *l, char *new_l)
+void error_hand(char **argv, char **array, int cmdnum, char *l, char *new_l)
 {
-	char *n;
+char *v;
 
-	n = _printint(cmdnum);
-	write(STDERR_FILENO, argv[0], str_len(argv[0]));
-	write(STDERR_FILENO, ": ", 2);
-	write(STDERR_FILENO, n, str_len(n));
-	write(STDERR_FILENO, ": ", 2);
-	write(STDERR_FILENO, arr[0], str_len(arr[0]));
-	write(STDERR_FILENO, ": ", 2);
-	write(STDERR_FILENO, "not found\n", 10);
-	free(n);
-	free_everything(l, new_l, arr);
-	exit(0);
+v = _printint(cmdnum);
+write(STDERR_FILENO, argv[0], str_len(argv[0]));
+write(STDERR_FILENO, ": ", 2);
+write(STDERR_FILENO, v, str_len(v));
+write(STDERR_FILENO, ": ", 2);
+write(STDERR_FILENO, array[0], str_len(array[0]));
+write(STDERR_FILENO, ": ", 2);
+write(STDERR_FILENO, "not found\n", 10);
+free(v);
+free_everything(l, new_l, array);
+exit(0);
 }
