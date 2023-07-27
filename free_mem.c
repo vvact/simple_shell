1cb39354e15268dd0c063b1f959a3927f04291ad
Victor Midega <vbm7752@gmail.com>
2023-07-27 15:20:24 -0700

    free_mem

--- free_mem.c
+++ free_mem.c
@@ -1,16 +1,16 @@
 #include "simple_shell.h"
 
 /**
- * free_everything - frees all var used
+ * free_mem - frees all var used
  * @l: user input
  * @new_l: user input without newline
- * @arr: double pointer to argument array
+ * @array: double pointer to argument array
  */
 
-void free_everything(char *l, char *new_l, char **arr)
+void free_mem(char *l, char *new_l, char **array)
 {
-	tkn_free(arr);
-	free(l); 
-	free(new_l);
-	free(arr);
+tkn_free(array);
+free(l);
+free(new_l);
+free(array);
 }
