80a84f42f3731aa6e266dc630994b7faac761f1b
Victor Midega <vbm7752@gmail.com>
2023-07-27 15:33:28 -0700

    memory realloc

--- realloc.c
+++ realloc.c
@@ -2,28 +2,27 @@
 
 /**
  * rea_lloc - memory reallocation for char
- * @d: pointer to an array of strings
+ * @v: pointer to an array of strings
  * Return: pointer to the reallocated mem
  */
-char *rea_lloc(char *d)
+char *rea_lloc(char *v)
 {
-	int k = 0;
-	char *new_pointer;
+int index = 0;
+char *new_pointer;
 
-	if (d == NULL)
-		return (NULL);
-
-	while (*(d + k) != '\n')
-		k++;
-	new_pointer = malloc(sizeof(char) * k + 1);
-	if (new_pointer == NULL)
-		return (NULL);
-	k = 0;
-	while (*(d + k) != '\n')
-	{
-		*(new_pointer + k) = *(d + k);
-		k++;
-	}
-	*(new_pointer + k) = '\0';
-	return (new_pointer);
+if (v == NULL)
+return (NULL);
+while (*(v + index) != '\n')
+index++;
+new_pointer = malloc(sizeof(char) * index + 1);
+if (new_pointer == NULL)
+return (NULL);
+index = 0;
+while (*(v + index) != '\n')
+{
+*(new_pointer + index) = *(v + index);
+index++;
+}
+*(new_pointer + index) = '\0';
+return (new_pointer);
 }
